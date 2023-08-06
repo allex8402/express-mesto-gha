e8c72232ad7bf097293c7f5426661c9f3779a168
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-08-06 18:56:03 +0800

    13 пр первая итерация, 21 загрузка

--- app.js
+++ app.js
@@ -41,6 +41,10 @@ app.use((err, req, res) => {
   return res.status(httpStatus.HTTP_STATUS_SERVER_ERROR).send({ message: 'Ошибка сервера' });
 });
 
+app.use((req, res) => {
+  res.status(httpStatus.HTTP_STATUS_NOT_FOUND).json({ message: 'Запрашиваемый ресурс не найден' });
+});
+
 // Запускаем сервер на заданном порту
 app.listen(PORT, () => {
   console.log(`Приложение слушает порт ${PORT}`);
