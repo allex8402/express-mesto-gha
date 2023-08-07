b1f7a6459601f0a33960cef7a4fbe392d31170d3
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-08-07 11:55:05 +0800

    13 пр вторая итерация, 3 загрузка

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+mongoose.connect(DB_URL, {
   useNewUrlParser: true,
 });
 
