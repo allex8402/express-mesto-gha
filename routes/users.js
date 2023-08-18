84da60f52eb7c76b19d1c425674cde9400622b06
allex8402 <120774992+allex8402@users.noreply.github.com>
2023-08-18 22:42:57 +0800

    14 пр первая итерация, 33 загрузка

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const { celebrate, Joi, Segments } = require('celebrate');
+
 const {
   getUsers, getUserById, updateProfile, updateAvatar, getUserInfo,
 } = require('../controllers/users');
 
 const usersRouter = express.Router();
 
+const updateProfileSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required(),
+  about: Joi.string().min(2).max(30).required(),
+});
+const updateAvatarSchema = Joi.object({
+  avatar: Joi.string().uri().required().error(new Error('Некорректный URL для аватара')),
+});
+
 usersRouter.get('/', getUsers);
-usersRouter.get('/:userId', getUserById);
+
+usersRouter.get('/:userId', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    userId: Joi.string().hex().length(24).required(),
+  }),
+}), getUserById);
+
 usersRouter.get('/me', getUserInfo);
-usersRouter.patch('/me', updateProfile);
-usersRouter.patch('/me/avatar', updateAvatar);
+
+usersRouter.patch('/me', celebrate({
+  [Segments.BODY]: updateProfileSchema,
+}), updateProfile);
+
+usersRouter.patch('/me/avatar', celebrate({
+  [Segments.BODY]: updateAvatarSchema,
+}), updateAvatar);
 
 module.exports = usersRouter;
